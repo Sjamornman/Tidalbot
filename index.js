dac6390328f6e5d12927b29b06919661270d9923
sittisak <sittisak@sittisaks-MacBook-Pro.local>
2016-04-24 14:22:16 +0700

    request

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser')
+var request = require('request')
 
 app.use(bodyParser.json())
 
@@ -28,6 +29,29 @@ app.post('/webhook/', function (req, res) {
   }
   res.sendStatus(200);
 });
+
+var token = "CAAZAK02hdpDEBAONx4XPkOrG3CTZBKbaZAgo6mZBdbLBRR7jRpmjhwkHjb7EiItsZAV5Ar0gZCZAjPrtXgSDXz1Cs2pz8qbXZC81iZAFguQKmhpCqGu93plG7V0kdZAWZBoOtCSxlLdp3lq0uwnY0azMBtoXIUuT2wRrjrxTYqO9ZBN7J64gZA0fpz3LZCZBcm88mqUPOACwCwRpVE4BAZDZD";
+
+function sendTextMessage(sender, text) {
+  var messageData = {
+    text:text
+  }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
 
 app.set('port',(process.env.PORT || 5000));
 
